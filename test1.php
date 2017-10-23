8a54c8ac306306804af9f461d04414622eb69f83
jeff99w <mckee99w@163.com>
2017-10-23 11:27:40 +0800

    Initial commit

---
+++ test1.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>这是一个测试
+		<?php 
+		echo "hello world";
+		?>
+	</h1>
+	
+</body>
+</html>
